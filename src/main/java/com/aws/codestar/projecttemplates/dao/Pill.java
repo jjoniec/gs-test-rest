65c3fda7cb0ac3fe1be3ac370bd6ded1279eff1f
jjoniec@yahoo.com <Turbacz!987a>
2019-12-27 14:50:13 -0700

    JPA dependencies

---
+++ src/main/java/com/aws/codestar/projecttemplates/dao/Pill.java
@@ -0,0 +1,23 @@
+package com.aws.codestar.projecttemplates.dao;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@AllArgsConstructor
+@ToString
+public class Pill {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String Description;
+    private Integer weight;
+
+    protected Pill() {}
+}
